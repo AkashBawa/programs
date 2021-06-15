a2654cd41c8b96d4eaff105b26ef2b724e841d0c
AkashBawa <bawa2027@gmail.com>
2021-06-15 19:15:59 +0530

    longest common subsequence

--- dynamic_programming/longestIncreasingSubSequence.cpp
+++ dynamic_programming/longestIncreasingSubSequence.cpp
@@ -38,28 +38,56 @@ void findSequence(int arr[], int size){
 		}
 	}
 	
-	cout<<"longest common subsequence is "<<overallMax<<endl;
+//	cout<<"longest common subsequence is "<<overallMax<<endl;
 	return;
 }
 
 
 // using dynamic programming;
-int longestIncreasingSubsequenceUsingDB(int arr[], int index, int size, int& max_ref){
+int longestIncreasingSubsequenceUsingDB(int arr[], int index, int size, int& max_ref, int list[]){
 	
+//	cout<<"list at index "<<index<<" is "<<list[index]<<endl;
+//	if(list[index] != 0){
+//		return list[index];
+//	}
 	if(index == size - 1){
+		list[index] = 1;
+//		cout<<"value returned at index "<<index<<" with value "<<arr[index]<<" is 1"<<endl;
 		return 1;
 	}
-	
+
 	int current_max;
-	int max = 0;
+	int max = 1;
+//	for(int i = index + 1; i < size; i++){
+//		if(arr[i] > arr[index]){
+//			current_max = longestIncreasingSubsequenceUsingDB(arr, i, size, max_ref, list) + 1;
+//			list[index] = current_max;
+//			if(max < current_max){
+//				max = current_max;
+//			}
+//		}
+//	}
+
 	for(int i = index + 1; i < size; i++){
-		if(arr[i] > arr[index]){
-			current_max = longestIncreasingSubsequence(arr, i, size, max_ref) + 1;
-			if(max < current_max){
-				max = current_max;
-			}
+		
+		if(list[i] != 0){
+			current_max = list[i];
+//			cout<<" not null "<<current_max<<endl;
+		} else {
+			list[i] = longestIncreasingSubsequenceUsingDB(arr, i, size, max_ref, list);
+			current_max  = list[i];
 		}
-	}
+		
+		if(arr[index] < arr[i]){
+			current_max++;
+			
+		}
+		if(current_max > max){
+			list[index] = current_max;
+			max = current_max;
+		}
+	}	
+//	cout<<"value returned at index "<<index<<" with value "<<arr[index]<<" is "<<max<<endl;
 	return max;
 }
 
@@ -67,10 +95,11 @@ void findSequenceUsingDB(int arr[], int size){
 	
 	int max = 1;
 	int overallMax = 0;
+	int list[size] = {0};
 	for(int i = 0; i < size; i++){
-		cout<<"start with "<<arr[i]<<endl;
-		max = longestIncreasingSubsequence(arr, i, size, max);
-		
+//		cout<<"start with "<<arr[i]<<endl;
+		max = longestIncreasingSubsequenceUsingDB(arr, i, size, max, list);
+		list[i] = max;
 		if(overallMax < max ){
 			overallMax = max;
 		}
@@ -84,9 +113,10 @@ int main(){
 	
 //	cin>>n;
 //	int arr[n + 1];
-	int arr[] = {3, 10, 2, 1, 20};
+	int arr[] = {3, 2};
 	int n = sizeof(arr) / sizeof(arr[0]);
-	findSequence(arr, n);
+//	findSequence(arr, n);
+	findSequenceUsingDB(arr, n);
 	return 0;
 	
 }
