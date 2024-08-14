5ca542a0dda47c61ee39c5585089bbfe0641c4ed
AkashBawa <bawa2027@gmail.com>
2024-08-14 13:26:06 -0700

    #7 with best practice

--- leetCode/150_Interviews_questions/7_buy_sell_stock.js
+++ leetCode/150_Interviews_questions/7_buy_sell_stock.js
@@ -1,38 +1,64 @@
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// var maxProfit = function(prices) {
+
+//     let minIndex = 0;
+//     let currentProfit = 0;
+//     let totalProfit = 0;
+//     for(let i = 1; i < prices.length; i++) {
+
+//         if(prices[i] <= prices[minIndex]) {
+            
+//             if(currentProfit > 0) {
+//                totalProfit +=  currentProfit;
+//                currentProfit = 0;
+//             }
+//             minIndex = i; 
+
+//         } else if( prices[i] > prices[minIndex] ) {
+
+//             let profit = prices[i] - prices[minIndex]
+//             if(profit > currentProfit) {
+//                 currentProfit = profit;
+//             } else if ( profit < currentProfit ) {
+//                 totalProfit +=  currentProfit;
+//                 minIndex = i;
+//                 currentProfit = 0;
+//             }
+//         }
+//     }
+
+//     if(currentProfit > 0) {
+//         totalProfit +=  currentProfit;
+//     }
+
+//     return totalProfit;
+// };
+
+
+//  Best practice
+
+
 /**
  * @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function(prices) {
 
-    let minIndex = 0;
-    let currentProfit = 0;
-    let totalProfit = 0;
-    for(let i = 1; i < prices.length; i++) {
+    let maxProfit = 0;
+    let start = prices[0];
 
-        if(prices[i] <= prices[minIndex]) {
-            
-            if(currentProfit > 0) {
-               totalProfit +=  currentProfit;
-               currentProfit = 0;
-            }
-            minIndex = i; 
-
-        } else if( prices[i] > prices[minIndex] ) {
-
-            let profit = prices[i] - prices[minIndex]
-            if(profit > currentProfit) {
-                currentProfit = profit;
-            } else if ( profit < currentProfit ) {
-                totalProfit +=  currentProfit;
-                minIndex = i;
-                currentProfit = 0;
-            }
+    for (let i = 1; i < prices.length; i++) {
+        
+        if( start < prices[i]) {
+            maxProfit += prices[i] - start;
         }
-    }
 
-    if(currentProfit > 0) {
-        totalProfit +=  currentProfit;
-    }
+        start = prices[i]; 
 
-    return totalProfit;
+    }  
+
+    return maxProfit;  
 };
