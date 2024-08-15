cce8dce8f29a889fa7dd9be1ee173afab4e376b6
AkashBawa <bawa2027@gmail.com>
2024-08-15 12:12:53 -0700

    #8 using dynamic programming

--- leetCode/150_Interviews_questions/8_jump_game.js
+++ leetCode/150_Interviews_questions/8_jump_game.js
@@ -1,31 +1,65 @@
+// Dymanic programming method
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 
-let isRechable = (nums, index) => {
-    
+let isRechable = (nums, index, newArr) => {
+
+    if(newArr[index] != null) {
+        return newArr[index];
+    }
     if(index == nums.length - 1 ) {
+        newArr[index] = true;
         return true
     }
     if(index > nums.length) return false;
 
     let firstVal = nums[index];
-    let isReached = false;
     for(let i = 1; i <= firstVal; i++){
-        let reached = isRechable(nums, index + i);
-        if( reached ) {
-            isReached = true;
-            break;
+        if(isRechable(nums, index + i, newArr)) {
+            return true;
         }
     }
-
-    return isReached;
+    newArr[index] = false;
+    return false;
 }
 
 
 var canJump = function(nums) {
-    return isRechable(nums, 0)
+    let newArr = [];
+    return isRechable(nums, 0, newArr)
 };
 
-console.log(canJump([2,3,1,1,4]))
+
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+
+// let isRechable = (nums, index) => {
+
+//     if(index == nums.length - 1 ) {
+//         return true
+//     }
+//     if(index > nums.length) return false;
+
+//     let firstVal = nums[index];
+//     let isReached = false;
+//     for(let i = 1; i <= firstVal; i++){
+//         let reached = isRechable(nums, index + i);
+//         if( reached ) {
+//             isReached = true;
+//             break;
+//         }
+//     }
+
+//     return isReached;
+// }
+
+
+// var canJump = function(nums) {
+//     return isRechable(nums, 0)
+// };
+
+// console.log(canJump([2,3,1,1,4]))
